5fc062ba31de9b9885911a1e6898d8e6727dffd0
Saniya Singh <81080993+Sana-Singh@users.noreply.github.com>
2021-08-25 11:20:43 +0100

    Update Grid.java

--- simulation/Grid.java
+++ simulation/Grid.java
@@ -1,12 +1,15 @@
-package simulation;
+ifYMoveSouthpackage simulation;
 
 import entities.Entity;
 import entities.Robot;
 
 import java.io.File;
 import java.util.*;
+import java.util.LinkedList;
+
 /**
  @author Josh
+ @author Saniya
  */
 public class Grid {
 
@@ -20,10 +23,8 @@ public class Grid {
         return map;
     }
 
-
-
-
     public Grid(){}
+
     public Grid(int width, int height){
         mapWidth=width;
         mapHeight=height;
@@ -50,6 +51,77 @@ public class Grid {
             robots.put(endPosition, moveRobot);
             return false;
         }
+    }
+
+    // Robot finds path by comapring the distance between each point. This is done by using Points to track each "Grid"Cell and then
+    // comparing each scenario where the Robot moves north, east, south or west.
+
+    public LinkedList<Point> findPath(Point current, Point endPosition) {
+
+      //create list of Points
+      LinkedList<Point> path = new LinkedList<Point>();
+
+      Point currentPosition = new Point(current.getX(), current.getY());
+
+      //while Robot not at detsination
+      while (currentPosition.getX() != endPosition.getX() || currentPosition.getY() != endPosition.getY()) {
+
+        //check x points (compare difference in distance if we move east or west)
+        if (currentPosition.getX() != endPosition.getX()) {
+          //if we go east
+          int ifXMoveEast = Math.abs(endPosition.getX() - currentPosition.east().getX());
+
+          //if we go west
+          int ifXMoveWest = Math.abs(endPosition.getX() - currentPosition.west().getX());
+
+          // go east if less distance
+          if (ifXMoveEast <= ifXMoveWest) {
+            Point newPoint = new Point(currentPosition.east().getX(), currentPosition.getY());
+
+            currentPosition = newPoint;
+
+            path.add(newPoint);
+          }
+
+          // go west if less distance
+          else {
+            Point newPoint = new Point(currentPosition.west().getX(), currentPosition.getY());
+
+            currentPosition = newPoint;
+
+            path.add(newPoint);
+          }
+        }
+
+        //check y points (compare difference in distance if we move north or south)
+        else if (currentPosition.getY() != endPosition.getY()) {
+
+          //if we move north
+          int ifYMoveNorth = Math.abs(endPosition.getY() - currentPosition.north().getY());
+
+          //if we move south
+          int ifYMoveSouth = Math.abs(endPosition.getY() - currentPosition.south().getY());
+
+          //go north if less distance
+          if (ifYMoveNorth <= ifYMoveSouth) {
+            Point newPoint = new Point(currentPosition.getX(), currentPosition.north().getY());
+
+            currentPosition = newPoint;
+
+            path.add(newPoint);
+          }
+
+          //go south if less disatance
+          else {
+            Point newPoint = new Point(currentPosition.getX(), currentPosition.south().getY());
+
+            currentPosition = newPoint;
+
+            path.add(newPoint);
+          }
+        }
+      }
+      return path;
     }
 
     public void generateDisplayGrid() {
